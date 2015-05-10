6e0252b09d448c7c5451c482aea84bc6fa2dac6e
lia978 <lia978@gmail.com>
2015-05-10 10:58:35 -0400

    changed path var in README.CBMRtools.R

--- README.CBMRtools.R
+++ README.CBMRtools.R
@@ -1,6 +1,7 @@
-CBMGIT <- Sys.getenv('CBMGIT')
+#CBMGIT <- Sys.getenv('CBMGIT')
+CBMGIT<-getwd()
 if (CBMGIT=="") stop( "Use 'setenv CBMGIT ..' to set CBMgithub's base directory" )
 
-setwd(paste(CBMGIT,'CBMRtools/CBMRtools/inst/package_make',sep=''))
+setwd(paste(CBMGIT,'/CBMRtools/inst/package_make',sep=''))
 source("CBMRtools.build.R") 
 
