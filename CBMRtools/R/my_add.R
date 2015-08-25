2ebcd5e6548a2f26959d6d3d97dcf8ad5c87d807
lia978 <lia978@gmail.com>
2015-08-25 17:41:25 -0400

    my_add

---
+++ CBMRtools/R/my_add.R
@@ -0,0 +1,13 @@
+#' \code{my_add} add two numbers and print it  
+#' @param x first number
+#' @param y second number
+#' @export
+my_add <- function(x, y)
+{
+	z <-x+y
+	print(z)
+	return(z)
+}
+
+
+
