634be64c75eaf8115c2eb28a944503dd2c4e06b6
Stefano Monti <smonti@smonti.bumc.bu.edu>
2017-12-04 10:31:32 -0500

    run_assign updates

--- dvlp/GeneSignature.R
+++ dvlp/GeneSignature.R
@@ -25,24 +25,30 @@ GeneSig <- setClass(
     slots = c(SigType = "character",
               MetaData = "data.frame",
               Signature = "data.frame"),
-    validity = function(object){
+    validity = function(object)
+    {
         errMessage = c()
-        if( sum( c("gene_rank", "gene_list") %in% object@SigType) != 1) errMessage <- c(errMessage,
-                                                                                        "SigType must be on of: 'gene_rank' or 'gene_list'.")
-        if( ncol(object@MetaData) != 1 ) errMessage <- c( errMessage,
-                                                         "MetaData must be 1 column." )
+        if( sum( c("gene_rank", "gene_list") %in% object@SigType) != 1) {
+            errMessage <- c(errMessage, "SigType must be on of: 'gene_rank' or 'gene_list'.")
+        }
+        if( ncol(object@MetaData) != 1 ) {
+            errMessage <- c( errMessage, "MetaData must be 1 column." )
+        }
         geneCheck <- c("ID", "P.Value", "Score", "high.class") %in% colnames(object@Signature)
-        if( !all(geneCheck) & object@SigType == "gene_rank" ) errMessage <- c( errMessage ,
-                                                                              paste( "Column(s): '",
-                                                                                    paste( c("ID", "P.Value", "Score", "high.class")[!geneCheck],
-                                                                                          collapse = "', '"),
-                                                                                    "' missing. Required when SigType = 'gene_rank'." ,
-                                                                                    sep = "") )
-        if( ncol(object@Signature) !=1 & object@SigType == "gene_list") errMessage <- c(errMessage,
-                                                                                        "If SigType = 'gene_list', SigType must be a single column.")
-        if( length(errMessage) == 0 ){
+        if( !all(geneCheck) & object@SigType == "gene_rank" ) {
+            errMessage <- c(errMessage,
+                            paste( "Column(s): '",
+                                  paste( c("ID", "P.Value", "Score", "high.class")[!geneCheck], collapse = "', '"),
+                                  "' missing. Required when SigType = 'gene_rank'." ,
+                                  sep = ""))
+        }
+        if( ncol(object@Signature) !=1 & object@SigType == "gene_list") {
+            errMessage <- c(errMessage, "If SigType = 'gene_list', SigType must be a single column.")
+        }
+        if( length(errMessage) == 0 ) {
             return(TRUE)
-        } else {
+        }
+        else {
             errMessage <- paste(errMessage, collapse = "\n")
             errMessage <- paste("\nCould not create GeneSig object:", errMessage, sep ="\n")
             return(errMessage)
@@ -60,7 +66,7 @@ GeneSig <- setClass(
 ##
 #######################################################################
 
-write.GeneSig <- function(SigObj, fileName, dir = getwd(), writeRDS = FALSE){
+write.GeneSig <- function(SigObj, fileName, dir = getwd(), writeRDS FALSE){
 
   if( class(SigObj) != "GeneSig" ) stop("SigObj must be of class 'GeneSig'.")
   outFile <- file.path(dir, fileName)
@@ -71,11 +77,10 @@ write.GeneSig <- function(SigObj, fileName, dir = getwd(), writeRDS = FALSE){
   colnames(MD) <- paste("#", "Metadata", sep = "")
   rownames(MD) <- paste("#", rownames(MD), sep = "")
 
-  write.table( MD, paste(outFile, "gsg", sep="."), row.names = TRUE, quote = FALSE, sep = "\t")
-  suppressWarnings( write.table( Sig, paste(outFile, "gsg", sep="."), row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t", append = TRUE) )
+  write.table( MD, paste(outFile, "gsg", sep="."), row.names=TRUE, quote=FALSE, sep = "\t")
+  suppressWarnings( write.table( Sig, paste(outFile, "gsg", sep="."), row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t", append=TRUE) )
 
-  if(writeRDS) saveRDS(SigObj, file = paste(outFile, "rds", sep = "."))
-
+  if (writeRDS) saveRDS(SigObj, file=paste(outFile, "rds", sep = "."))
 }
 
 
